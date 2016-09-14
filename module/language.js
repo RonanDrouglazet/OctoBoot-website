e691b8198a8655ba0fdc698bd278425dd91bdd48
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2016-09-14 22:35:16 +0200

    language

--- module/language.js
+++ module/language.js
@@ -1,9 +1,9 @@
 (window.OctoBoot_plugins = window.OctoBoot_plugins || {}).language = function() {
     if (location.hash) {
-        var elements = $('*[ob-language-' + location.hash + ']')
+        var elements = $('*[ob-language-' + location.hash.substr(1) + ']')
         if (elements.length) {
             elements.each(function(i, el) {
-                el.innerHTML = el.innerHTML.replace($(el).attr('ob-language-default'), $(el).attr('ob-language-' + location.hash))
+                el.innerHTML = el.innerHTML.replace($(el).attr('ob-language-default'), $(el).attr('ob-language-' + location.hash.substr(1)))
             })
         }
     }
